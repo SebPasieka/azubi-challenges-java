f3285718791d552cbf33586034deb9eac25342a3
SebPasieka <sebpasieka@gmail.com>
2021-03-24 13:03:19 +0100

    #4 use hashmap to implement

--- src/main/java/azubi/challenges/task002/UniqueCharsTester.java
+++ src/main/java/azubi/challenges/task002/UniqueCharsTester.java
@@ -1,5 +1,8 @@
 package azubi.challenges.task002;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UniqueCharsTester {
 
     /**
@@ -10,20 +13,24 @@ public class UniqueCharsTester {
      */
     public static boolean hasUniqueChars(String candidate) {
         int candidateLength = candidate.length();
-        if (candidateLength > 256) return false;
         if (candidateLength <= 1) return true;
 
-        boolean[] checkingArray = new boolean[256];
+        char[] charOfCandidate = candidate.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
 
         for (int i = 0; i < candidateLength; i++) {
-
-            char value = candidate.charAt(i);
-
-            if (checkingArray[value]) return false;
-
-            checkingArray[value] = true;
+            if (!map.containsKey(charOfCandidate[i])) {
+                map.put(charOfCandidate[i], 1);
+            } else {
+                int oldValue = map.get(charOfCandidate[i]);
+                int newValue = ++oldValue;
+                map.put(charOfCandidate[i], newValue);
+            }
         }
-
+        for (Map.Entry<Character, Integer> data : map.entrySet()) {
+            if (data.getValue() > 1) return false;
+        }
         return true;
     }
 }
